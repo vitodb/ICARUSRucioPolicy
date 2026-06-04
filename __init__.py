74867b398d5053dcf37902d2d22a94cb843f6796
Vito Di Benedetto <55766483+vitodb@users.noreply.github.com>
2026-06-04 18:07:30 -0500

    Fix import

--- __init__.py
+++ __init__.py
@@ -3,7 +3,7 @@
 # Brandon White <bjwhite@fnal.gov>, 2020
 
 from .path_gen import construct_surl_icarus
-from .lfn2pfn import lfn2pfn_ICARUS
+from .lfn2pfn import lfn2pfn_SLAC_ICARUS
 
 SUPPORTED_VERSION=[">=36.0"]
 
