80ac370bf2a70615820c240a862285c3f9b0b59b
Vito Di Benedetto <55766483+vitodb@users.noreply.github.com>
2026-06-04 14:27:12 -0500

    Initialize params

--- lfn2pfn.py
+++ lfn2pfn.py
@@ -7,16 +7,19 @@ def lfn2pfn_SLAC_ICARUS(scope, name, rse, rse_attrs, protocol_attrs):
 
     didclient = None
     didmd = {}
+    guid = ''
+    dsetl = None
+    dsetprefix = ''
+    didclient = DIDClient()
     didmd = didclient.get_metadata(scope,name)
     guid = didmd.get("guid")
     dsetl = didclient.get_dataset_by_guid(guid)
     dsetprefix = list(dsetl)[0].get("name")
-    print(dsetprefix)
 
     hs = hashlib.sha256(name.encode('utf-8')).hexdigest()
 
     pfn = os.path.join(
-        scope,
+        scope.replace('.', '/'),
         dsetprefix,
         hs[0:2],
         hs[2:4],
